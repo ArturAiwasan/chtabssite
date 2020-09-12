47bbd7358ffbe1b3afd6cc1762c774c29c16c5c2
ArturAiwasan <root@artur-pc>
2020-09-12 09:55:35 +0300

    020

---
+++ wp-content/themes/ntt/template-parts/entity-main-header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Entity Main Header
+ */
+get_header();
+?>
+
+<div class="ntt--entity-main-header ntt--cn" data-name="Entity Main Header">
+    <?php ntt__tag__entity_view_heading(); ?>
+</div>
